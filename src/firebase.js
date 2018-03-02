80c301a5d75713874fb5bf48201d3fc5f4914596
Allison Tarr <hello@allisontarr.com>
2018-03-01 21:58:05 -0500

    firebase init

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+	apiKey: 'AIzaSyBmDVAyiu26jyaAM1UHRNdmNW5GtO-gGXE',
+	authDomain: 'coding-anxiety.firebaseapp.com',
+	databaseURL: 'https://coding-anxiety.firebaseio.com',
+	projectId: 'coding-anxiety',
+	storageBucket: 'coding-anxiety.appspot.com',
+	messagingSenderId: '758786716395'
+};
+firebase.initializeApp(config);
+export default firebase;
