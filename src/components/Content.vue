dc5a608c4a7f7c5ad4d7fa768f95c04f9b8d78bc
Allison Tarr <hello@allisontarr.com>
2018-05-05 22:55:55 -0400

    reorg form + content ordering + restyle

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -3,19 +3,22 @@
       <section class="left">
         <div class="wrapper">
           <header>
-            <h1>{{ msg }}</h1>
-
-            <div class="social clearfix">
-              <a href="https://www.instagram.com/codingwithanxiety/"><i class="fab fa-instagram"></i></a>
-              <a href="http://www.twitter.com/codinganxiety"><i class="fab fa-twitter"></i></a>
-            </div> <!--.social-->
+            <div class="line">
+              <p><span class="now">Last login: {{ dateTime }}</span></p>
+            </div>
+            <div class="line">
+              <span class="path">~/<h1>{{ msg }}</h1></span>
+              <span class="branch"><i class="fas fa-code-branch"></i> master</span>
+              <span class="command"></span>
+            </div>
           </header>
-
-          <div class="anxiety-wrapper">
-            <List/>
-            <Form/>
-          </div><!--.anxiety-wrapper-->
         </div> <!--.wrapper-->
+
+        <List/>
+
+        <div class="anxiety-wrapper">
+          <Form/>
+        </div><!--.anxiety-wrapper-->
       </section>
 
       <Instafeed/>
@@ -40,19 +43,109 @@ export default {
   },
   data() {
     return {
-      msg: 'Coding with Anxiety',
+      msg: 'Coding-with-Anxiety',
+      dateTime: getDateTime(),
     };
   },
 };
+
+function getDateTime() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const day = now.getDate();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
+
+    if (second <= 10) {
+        var seconds = '0' + second;
+    } else {
+        var seconds = second;
+    }
+
+    const weekDay = [
+      'Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat',
+    ];
+    const today = weekDay[now.getDay()];
+
+    const monthName = [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May',
+      'Jun', 'Jul', 'Aug', 'Sept', 'Oct',
+      'Nov', 'Dec',
+    ];
+    const thisMonth = monthName[now.getMonth()];
+
+    return today + ' ' + thisMonth + ' ' + day + ' ' + year + ' ' + hour + ':' + minute + ':' + seconds;
+}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import '../assets/styles/_variables.scss';
 
+h1 {
+  display: inline-block;
+  font-size: 20px;
+  margin: 0;
+}
+
+.line:nth-of-type(1) {
+  opacity: 0.75;
+}
+
+.line:nth-of-type(2) {
+  font-size: 0;
+  height: 1.5rem;
+}
+
+.path,
+.branch {
+  display: inline-block;
+  font-size: 20px;
+  color: $white;
+}
+
+.path {
+  background-color: $zsh-path-background;
+  padding: 0 10px;
+}
+
+.branch {
+  background-color: #fed888;
+  padding-right: 12px;
+  color: $bg;
+
+  &:before {
+    border: 14px solid transparent;
+    border-left-color: $zsh-path-background;
+    content: '';
+    display: inline-block;
+    height: 0;
+    vertical-align: -7px;
+    width: 0;
+  }
+}
+
+.command {
+  &:before {
+      border: 16px solid transparent;
+      border-left-color: #fed888;
+      content: '';
+      display: inline-block;
+      height: 0;
+      vertical-align: -9px;
+      width: 0;
+  }
+}
+
+.anxiety-wrapper {
+  margin-top: 120px;
+}
+
 // Social icons.
 .social {
-  margin-bottom: 20px;
+  margin: 20px 0;
 
   a {
     color: $white;
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,7 +2,8 @@
   <section class="wrapper-form">
     <form id="anxiety-share" class="anxiety-form" v-on:submit.prevent="addAnxiety">
       <label for="Your Anxiety">Submit Your Anxiety</label>
-      <input type="text" id="yourAnxiety" v-model="newAnxiety.thought">
+       <span class="data-prompt"><i class="fas fa-angle-double-right"></i></span><span class="blinking-cursor"></span>
+       <input type="text" id="yourAnxiety" v-model="newAnxiety.thought">
     </form>
   </section>
 </template>
@@ -39,10 +40,44 @@ export default {
 @import '../assets/styles/_variables.scss';
 
 .anxiety-form {
-  margin-top: 50px;
+  border-top: 1px dotted $zsh-text;
+  border-bottom: 1px dotted $zsh-text;
+  padding: 3px 0;
+
+  .data-prompt {
+    color: $zsh-text;
+    vertical-align: super;
+    font-size: 18px;
+  }
+
+  .blinking-cursor {
+    @include size(2px 25px);
+
+    animation-name: blinker;
+    animation-iteration-count: infinite;
+    animation-timing-function: cubic-bezier(1,0,0,1);
+    animation-duration: 1s;
+    background: $zsh-text;
+    border-radius: 3px;
+    display: inline-block;
+    margin-left: 6px;
+  }
+
+  @keyframes blinker {
+    from { opacity: 1.0; } to { opacity: 0.0; }
+  }
 
   label {
-    margin-right: 16px;
+    display: block;
+  }
+
+  input {
+    background-color: $bg;
+    border-radius: 0;
+    border: none;
+    box-shadow: none;
+    vertical-align: middle;
+    white-space: normal;
   }
 }
 
